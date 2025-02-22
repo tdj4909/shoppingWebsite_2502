bfd92d6f465acaf65604bc591b2a29aa1584456d
tdj4909 <tdj4909@gmail.com>
2025-02-22 23:37:35 +0900

    update

--- backend/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ backend/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
@@ -30,6 +31,12 @@ public class ArticleController {
         Article saved = articleRepository.save(article);
         log.info(saved.toString());
 
+        return "";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id){
+        log.info("id = " + id);
         return "";
     }
 }
