a3fd95a7f6881b7242120b8e33f0918f9c7b99d7
tdj4909 <tdj4909@gmail.com>
2025-02-23 22:12:19 +0900

    update

--- backend/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ backend/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -56,4 +56,31 @@ public class ArticleController {
 
         return "articles/index";
     }
+
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id, Model model){
+
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        model.addAttribute("article", articleEntity);
+
+        return "articles/edit";
+    }
+
+    @PostMapping("articles/update")
+    public String update(ArticleForm form){
+
+        log.info(form.toString());
+
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+
+        if (target != null){
+            articleRepository.save(articleEntity);
+        }
+
+        return "redirect:/articles/" + articleEntity.getId();
+    }
 }
