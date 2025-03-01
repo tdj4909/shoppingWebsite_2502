c52e4031b10d251ed111cc705af0984888cb0a4c
tdj4909 <tdj4909@gmail.com>
2025-03-02 00:46:12 +0900

    update

--- backend/src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ backend/src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -1,12 +1,13 @@
 package com.example.firstproject.api;
 
+import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.service.ArticleService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,30 +28,24 @@ public class ArticleApiController {
         return articleService.show(id);
     }
 
-//    @PostMapping("/api/articles")
-//    public Article create(@RequestBody ArticleForm dto){
-//        Article article = dto.toEntity();
-//        return articleRepository.save(article);
-//    }
-//
-//    @PatchMapping("/api/articles/{id}")
-//    public ResponseEntity<Article> update(@PathVariable Long id,
-//                                 @RequestBody ArticleForm dto){
-//        Article article = dto.toEntity();
-//        log.info("id: {}, article: {}", id, article.toString());
-//
-//        Article target = articleRepository.findById(id).orElse(null);
-//
-//        if (target == null || id != article.getId()){
-//            log.info("잘못된 요청! id: {}, article: {}", id, article.toString());
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-//        }
-//
-//        target.patch(article);
-//        Article updated = articleRepository.save(target);
-//        return ResponseEntity.status(HttpStatus.OK).body(updated);
-//    }
-//
+    @PostMapping("/api/articles")
+    public ResponseEntity<Article> create(@RequestBody ArticleForm dto){
+        Article created = articleService.create(dto);
+
+        return (created != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(created) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @PatchMapping("/api/articles/{id}")
+    public ResponseEntity<Article> update(@PathVariable Long id,
+                                 @RequestBody ArticleForm dto){
+        Article updated = articleService.update(id, dto);
+        return (updated != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(updated) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
 //    @DeleteMapping("/api/articles/{id}")
 //    public ResponseEntity<Article> delete(@PathVariable Long id){
 //
--- backend/src/main/java/com/example/firstproject/service/ArticleService.java
+++ backend/src/main/java/com/example/firstproject/service/ArticleService.java
@@ -1,12 +1,15 @@
 package com.example.firstproject.service;
 
+import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class ArticleService {
     @Autowired
@@ -18,5 +21,29 @@ public class ArticleService {
 
     public Article show(Long id) {
         return articleRepository.findById(id).orElse(null);
+    }
+
+    public Article create(ArticleForm dto) {
+        Article article = dto.toEntity();
+        if (article.getId() != null){
+            return null;
+        }
+        return articleRepository.save(article);
+    }
+
+    public Article update(Long id, ArticleForm dto) {
+        Article article = dto.toEntity();
+        log.info("id: {}, article: {}", id, article.toString());
+
+        Article target = articleRepository.findById(id).orElse(null);
+
+        if (target == null || id != article.getId()){
+            log.info("잘못된 요청! id: {}, article: {}", id, article.toString());
+            return null;
+        }
+
+        target.patch(article);
+        Article updated = articleRepository.save(target);
+        return updated;
     }
 }
