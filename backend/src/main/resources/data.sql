9a0dcf00db5b7dbcef064ca4054afb4c312b433d
tdj4909 <tdj4909@gmail.com>
2025-03-03 19:25:52 +0900

    update

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,3 +1,4 @@
-INSERT INTO article(id, title, content) VALUES(1, '가가가가', '1111');
-INSERT INTO article(id, title, content) VALUES(2, '나나나', '222');
-INSERT INTO article(id, title, content) VALUES(3, '다다', '33');
+INSERT INTO article(title, content) VALUES('가가가가', '1111');
+INSERT INTO article(title, content) VALUES('나나나', '222');
+INSERT INTO article(title, content) VALUES('다다', '33');
+
