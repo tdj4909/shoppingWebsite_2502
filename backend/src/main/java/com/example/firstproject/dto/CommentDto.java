b3564200f3bd31ba95559d94603317b99d9171d4
tdj4909 <tdj4909@gmail.com>
2025-03-03 22:15:20 +0900

    update

---
+++ backend/src/main/java/com/example/firstproject/dto/CommentDto.java
@@ -0,0 +1,17 @@
+package com.example.firstproject.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class CommentDto {
+    private Long id;
+    private Long articleId;
+    private String nickname;
+    private String body;
+}
